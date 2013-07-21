27deb763cf58e4d8f1ddd9be0df2cd695d38a4bf
Dustin Bolink <williambolink@gmail.com>
2013-07-21 15:06:29 -0700

    implemented a Move class

--- src/model/Move.java
+++ src/model/Move.java
@@ -4,6 +4,12 @@ public class Move {
 	public Pawn pawn;
 	public int startPosition;
 	public Player collision;
+	
+	public Move(){
+		this.pawn = null;
+		this.collision = null;
+		this.startPosition = 0;
+	}
 
 	public Move(Pawn pawn, Player collision, int startPosition){
 		this.pawn = pawn;
